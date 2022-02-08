e9f2d1dc52be1f44e0d92ca657426d25a798383b
tebogomotibane <tebogomotibane@gmail.com>
2022-02-08 13:24:33 +0200

    changed variable declaration to const

--- Task0.7.js
+++ Task0.7.js
@@ -1,8 +1,8 @@
 function convertToFahrenheit(celcius) {
-  let fanhre = celcius * 1.8 + 32;
+  const fanhre = celcius * 1.8 + 32;
   return fanhre;
 }
 function convertToCelcius(fanhre) {
-  let celcius = (fanhre - 32) * 0.5556;
+  const celcius = (fanhre - 32) * 0.5556;
   return celcius.toFixed(2); //rounds to 2 decimal places
 }
