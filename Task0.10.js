56b51f165062a3a18e380ef0b3f0464d0e8dffe9
tebogomotibane <tebogomotibane@gmail.com>
2022-02-08 18:30:58 +0200

    modified to separate output by commas

--- Task0.10.js
+++ Task0.10.js
@@ -1,11 +1,11 @@
 function getCommomCharacters(word1, word2) {
-  let commonLetters = "";
+  let commonLetters = [];
   for (let char in word1)
     if (
       word2.includes(word1[char]) &&
       commonLetters.includes([word1[char]]) == false // ignores duplicates
     ) {
-      commonLetters += word1[char];
+      commonLetters.push(word1[char]);
     }
-  console.log("Common letters:", commonLetters);
+  console.log("Common letters:", commonLetters.toString());
 }
