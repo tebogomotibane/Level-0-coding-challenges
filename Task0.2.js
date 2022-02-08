2db0a9419d24187b44f2ddc232197f8a5acf22f7
tebogomotibane <tebogomotibane@gmail.com>
2022-02-08 13:09:29 +0200

    changed variables from let to const declaration

--- Task0.2.js
+++ Task0.2.js
@@ -1,6 +1,6 @@
-let x = 1 + 1 * 2;
-let y = (1 + 1) * 2;
-let z = 1 + 1 * 2;
-let a = 1 + (1 * 2) / 2;
-let b = (1 + 1 * 2) / 2;
+const x = 1 + 1 * 2;
+const y = (1 + 1) * 2;
+const z = 1 + 1 * 2;
+const a = 1 + (1 * 2) / 2;
+const b = (1 + 1 * 2) / 2;
 console.log(x, y, z, a, b);
