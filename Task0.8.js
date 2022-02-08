887964e1e4dc45e18af393d80f6638cb7a0b5f0e
tebogomotibane <tebogomotibane@gmail.com>
2022-02-08 18:46:12 +0200

    declared variables with const

--- Task0.8.js
+++ Task0.8.js
@@ -2,8 +2,8 @@ function convertToTime(number) {
   const hours = Math.floor(number / 60);
   const minutes = number % 60;
 
-  const getHours = (hours === 1) ? "hour" : "hours";
-  const getMinutes = (minutes ===1 ) ? "minute" : "minutes";
+  const getHours = (hours === 1 )? "hour" : "hours";
+  const getMinutes = (minutes === 1) ? "minute" : "minutes";
 
   return hours + " " + getHours + "," + " " + minutes + " " + getMinutes;
 }
