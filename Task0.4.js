72dd067af2ebab739f79372ad5a5535c1e7c3c27
tebogomotibane <tebogomotibane@gmail.com>
2022-02-08 13:21:08 +0200

    used strict comparison,modified if-else statement

--- Task0.4.js
+++ Task0.4.js
@@ -1,10 +1,7 @@
 function evenOrOdd(integer) {
-  if (isNaN(integer) || Number.isInteger(integer) == false) {
-    //validates input
-    console.log(undefined);
-  } else if (integer % 2 == 0) {
+  if (integer % 2 === 0) {
     console.log("even");
-  } else if (integer % 2 != 0) {
+  } else {
     console.log("odd");
   }
 }
